5355c7bf9df1e44bcb4f9fae08844e4f74f4f948
Bjørnar Øvrebø <bjornarovrebo@bjrnars-mbp.lan>
2020-12-04 01:01:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// question 1
+
+var firstVar 
+console.log(firstVar);
+
+// question 2
+
+var firstName = ("Bjørnar");
+console.log(firstName);
+
+// question 3
+
+var questionThree = 3;
+console.log(3);
+
+// question 4
+
+var division = 20/5;
+console.log(division);
+
+// question 5
+
+var animal = ("frog");
+console.log(animal);
+
+typeof animal;
+
+// question 6 
+
+var orderHasShipped = true
+console.log(orderHasShipped);
+
+// question 7
+
+var orderHasShipped = false
+
+if(orderHasShipped===true) {
+console.log("True");
+}
+else {
+    console.log("False");
+}
+
+//question 8 
+
+for( var i = 0 ; i<=9 ; i++ ) {
+    console.log(i);
+}
